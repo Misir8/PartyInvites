1f516de44ae17814416b24d2dd4b8caa11f6a230
Misir <misirua.code@edu.az>
2019-10-13 00:33:19 +0400

    add first backend app

---
+++ Partyinvites/Models/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Partyinvites.Models
+{
+    public class Repository
+    {
+        public static List<GuestResponse> Responses { get; } = new List<GuestResponse>();
+        public static void AddResponse(GuestResponse response)
+        {
+            Responses.Add(response);
+        }
+    }
+}
